7073074fc580090a9cd69bdcb2b042a90afb0748
bartek 'basz' szurgot <git@baszerr.eu>
2019-06-04 21:02:25 +0200

    added commandline-controlled preview/no-preview window mode

--- control-sw/face_follower.py
+++ control-sw/face_follower.py
@@ -2,8 +2,30 @@
 import sys
 import cv2
 import time
+import getopt
 import Servo
 import Tracker
+
+
+def parseCmdLine():
+    opts, args = getopt.getopt(sys.argv[1:], "hpn", ["help", "preview", "no-preview"])
+    if len(args) > 0:
+        raise Exception("unknown argument: '" + args[0] + "'")
+    preview = None
+    for opt, arg in opts:
+        if opt == "-h" or opt == "--help":
+            sys.stdout.write(sys.argv[0] + " {--help|--preview|--no-preview}\n")
+            sys.exit(0)
+        if opt == "-p" or opt == "--preview":
+            preview = True
+            continue
+        if opt == "-n" or opt == "--no-preview":
+            preview = False
+            continue
+    if preview is None:
+        raise Exception("preview must be xplicitly enabled or disabled")
+    return preview
+
 
 def drawRectangles(faces, img):
     for (x,y,w,h) in faces:
@@ -13,6 +35,8 @@ def imageCenter(img):
     h,w,channels = img.shape
     return (w/2, h/2)
 
+previewWindow = parseCmdLine()
+
 lineDrv  = Servo.LineDriver('/dev/ttyUSB0')
 position = Servo.Position(lineDrv, 'l', 'f')
 tracker  = Tracker.Tracker(position)
@@ -21,8 +45,9 @@ tracker  = Tracker.Tracker(position)
 faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 frameGrabber = cv2.VideoCapture(0)
 
-cv2.namedWindow("preview", cv2.WND_PROP_FULLSCREEN)
-cv2.setWindowProperty("preview", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
+if previewWindow:
+    cv2.namedWindow("preview", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("preview", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
 
 ret    = 0
 errors = 0
@@ -46,8 +71,9 @@ while True:
         gray     = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         center   = imageCenter(img)
         faces    = faceDetector.detectMultiScale(gray, 1.3, 5)
-        drawRectangles(faces, img)
-        cv2.imshow("preview",img)
+        if previewWindow:
+            drawRectangles(faces, img)
+            cv2.imshow("preview",img)
 
         tracker.updatePositionForFaces(faces, center)
         sys.stderr.write('#')
